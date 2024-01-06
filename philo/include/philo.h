7ff831f1afdca8b2b53024119424b3f29bfcd74c
Andrei Basdereff <114996380+Neffi42@users.noreply.github.com>
2024-01-06 14:05:11 +0100

    first commit

---
+++ philo/include/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/06 13:54:18 by abasdere          #+#    #+#             */
+/*   Updated: 2024/01/06 14:04:10 by abasdere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
