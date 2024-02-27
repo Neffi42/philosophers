159de4d9c52f9b67616ccb65d1ec52d24accb5b3
Andrei Basdereff <114996380+Neffi42@users.noreply.github.com>
2024-02-27 17:50:29 +0100

    norm

--- philo_bonus/include/philo_bonus.h
+++ philo_bonus/include/philo_bonus.h
@@ -6,12 +6,12 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 13:54:18 by abasdere          #+#    #+#             */
-/*   Updated: 2024/01/06 19:07:12 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/02/27 17:50:10 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
+#ifndef PHILO_BONUS_H
+# define PHILO_BONUS_H
 
 # include <stdio.h>
 # include <stdlib.h>
