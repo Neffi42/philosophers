a1fc40827a53335d4231f57a7e7211526addff79
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-02-29 10:46:19 +0100

    corrected error message

--- philo_bonus/src/routine_utils2.c
+++ philo_bonus/src/routine_utils2.c
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 10:35:35 by abasdere          #+#    #+#             */
-/*   Updated: 2024/02/29 10:40:47 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/02/29 10:45:07 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*one_philo(t_philo *philo)
 	if (gettimeofday(&tv, NULL) && error(FCT, "gettimeofday"))
 		return (sem_post(philo->sems->run), NULL);
 	time = tv.tv_sec * 1000000 + tv.tv_usec - philo->rules->s_time;
-	printf("%ld %u died\n", time / 1000, philo->id);
+	printf("%ld %u has taken a fork\n", time / 1000, philo->id);
 	usleep(philo->rules->time_die);
 	if (gettimeofday(&tv, NULL) && error(FCT, "gettimeofday"))
 		return (sem_post(philo->sems->run), NULL);
